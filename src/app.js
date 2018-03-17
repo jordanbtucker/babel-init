b2aa3fffbe378a846717bd71c107ecf20a4daca0
Jordan Tucker <jordanbtucker@gmail.com>
2018-03-17 01:50:40 -0500

    Use debug

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
+import Debug from 'debug'
+
+const debug = Debug('babel-init')
+
 export default class App {
 	async run () {
 		const message = await Promise.resolve(`Hello, ${process.env.NAME}!`)
+		debug('message: %s', message)
 		console.log(message)
 	}
 }
